db5fa9c820d1e13023a3c5088322b1d388e91907
khaideptraine <khaipham1701@gmail.com>
2021-11-13 18:14:48 +0700

    git-nhu-buoi

--- du-an-1/app/Http/Controllers/ProductDetailController.php
+++ du-an-1/app/Http/Controllers/ProductDetailController.php
@@ -20,7 +20,13 @@ class ProductDetailController extends Controller
                         ->select('variant.Color')
                         ->where('Slug','=', $slug)
                         ->Get();
-//         dd($data);
-        return view('shop/productdetail', compact('data','variant'));
+        $comment = DB::table('comment')
+                        ->join('product', 'product.ProductId', '=', 'comment.ProductId')
+                        ->join('users','users.UserId', '=', 'comment.UserId')
+                        ->select('comment.*','product.Slug','users.Fullname')
+                        ->where('product.Slug','=', $slug)
+                        ->Get();
+//         dd($comment);
+        return view('shop/productdetail', compact('data','variant','comment'));
     }
 }
--- du-an-1/app/Providers/AppServiceProvider.php
+++ du-an-1/app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\DB;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -24,6 +24,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $category = DB::table('Category')->where('CatActive','=',1)->Get();
+//        $countpd = DB::table('category')
+//            ->join('product', 'category.CategoryId','=','product.CategoryId')
+//            ->select('product.*','category.CategoryId')
+//            ->where('CatActive','=',1)
+//            ->where('Active','=',1)
+//            ->Get();
+//        dd($countpd);
+        view()->share(compact('category'));
         Paginator::useBootstrap();
+
     }
 }
