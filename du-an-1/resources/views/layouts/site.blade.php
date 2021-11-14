801dae75415c23c58e1c3fce90cd476bf2963aac
khaideptraine <khaipham1701@gmail.com>
2021-11-14 19:24:11 +0700

    upadate comment

--- du-an-1/resources/views/layouts/site.blade.php
+++ du-an-1/resources/views/layouts/site.blade.php
@@ -32,6 +32,8 @@
 
     <link rel="stylesheet" href="{{ asset('css/index.css')}}"/>
     <link rel="stylesheet" href="{{ asset('css/plugins/swiper.min.css')}} "/>
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
 </head>
 
 <body>
