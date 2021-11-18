4d883413e7e80a44feb17a9e7231015f216484b2
khaideptraine <khaipham1701@gmail.com>
2021-11-18 18:29:19 +0700

    update theme

--- du-an-1/resources/views/layouts/admin.blade.php
+++ du-an-1/resources/views/layouts/admin.blade.php
@@ -68,51 +68,66 @@
         <div class="menu-list">
             <nav class="navbar navbar-expand-lg navbar-light">
                 <a class="d-xl-none d-lg-none" href="#">Dashboard</a>
-                <button
-                    class="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarNav"
-                    aria-controls="navbarNav"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav flex-column">
                         <li class="nav-divider">Menu</li>
                         <li class="nav-item">
-                            <a class="nav-link active" href="#" aria-expanded="false"
+                            <a class="nav-link" href="#" aria-expanded="false"
                             ><i class="fa fa-fw fa-user-circle"></i>Dashboard
                                 <span class="badge badge-success">6</span></a
                             >
                         </li>
                         <li class="nav-item">
-                            <a
-                                class="nav-link"
-                                href="#"
-                                data-toggle="collapse"
-                                aria-expanded="false"
-                                data-target="#submenu-10"
-                                aria-controls="submenu-10"
-                            ><i class="fas fa-f fa-folder"></i>Menu Level</a
-                            >
+                            <a  class="nav-link" href="#" data-toggle="collapse"
+                                aria-expanded="false" data-target="#submenu-10" aria-controls="submenu-10">
+                                <i class="fas fa-f fa-folder"></i>Menu Level
+                            </a>
                             <div id="submenu-10" class="collapse submenu" style="">
                                 <ul class="nav flex-column">
                                     <li class="nav-item">
                                         <a class="nav-link" href="#">Level 1</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a
-                                            class="nav-link"
-                                            href="#"
-                                            data-toggle="collapse"
-                                            aria-expanded="false"
-                                            data-target="#submenu-11"
-                                            aria-controls="submenu-11"
-                                        >Level 2</a
-                                        >
+                                        <a class="nav-link" href="#" data-toggle="collapse"
+                                           aria-expanded="false" data-target="#submenu-11" aria-controls="submenu-11">
+                                            Level 2
+                                        </a>
+                                        <div id="submenu-11" class="collapse submenu" style="">
+                                            <ul class="nav flex-column">
+                                                <li class="nav-item">
+                                                    <a class="nav-link" href="#">Level 1</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a class="nav-link" href="#">Level 2</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Level 3</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a  class="nav-link" href="#" data-toggle="collapse"
+                                aria-expanded="false" data-target="#submenu-9" aria-controls="submenu-9">
+                                <i class="fas fa-f fa-folder"></i>Menu Level
+                            </a>
+                            <div id="submenu-9" class="collapse submenu" style="">
+                                <ul class="nav flex-column">
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Level 1</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#" data-toggle="collapse"
+                                           aria-expanded="false" data-target="#submenu-11" aria-controls="submenu-11">
+                                            Level 2
+                                        </a>
                                         <div id="submenu-11" class="collapse submenu" style="">
                                             <ul class="nav flex-column">
                                                 <li class="nav-item">
